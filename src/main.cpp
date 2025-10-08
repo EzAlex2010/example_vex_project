291a701731c3322ca73c65d9f689b9324a340851
EzAlex2010 <ezekielcreeger@gmail.com>
2025-10-08 10:54:00 -0400

    simplification

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,8 @@
 pros::Controller controller(pros::E_CONTROLLER_MASTER);
 
 // motor groups
-pros::MotorGroup leftMotors({-5, 4, -3},
-                            pros::MotorGearset::blue); // left motor group - ports 3 (reversed), 4, 5 (reversed)
-pros::MotorGroup rightMotors({6, -9, 7}, pros::MotorGearset::blue); // right motor group - ports 6, 7, 9 (reversed)
-
+pros::MotorGroup leftMotors({-5, 4, -3},pros::MotorGearset::blue);
+pros::MotorGroup rightMotors({6, -9, 7}, pros::MotorGearset::blue);
 // Inertial Sensor on port 10
 pros::Imu imu(10);
 
@@ -33,30 +31,30 @@ lemlib::Drivetrain drivetrain(&leftMotors, // left motor group
 );
 
 // lateral motion controller
-lemlib::ControllerSettings linearController(10, // proportional gain (kP)
+lemlib::ControllerSettings linearController(0.5, // proportional gain (kP)
                                             0, // integral gain (kI)
-                                            3, // derivative gain (kD)
-                                            3, // anti windup
+                                            0.1, // derivative gain (kD)
+                                            0, // anti windup
                                             1, // small error range, in inches
-                                            100, // small error range timeout, in milliseconds
-                                            3, // large error range, in inches
-                                            500, // large error range timeout, in milliseconds
-                                            20 // maximum acceleration (slew)
+                                            0, // small error range timeout, in milliseconds
+                                            0, // large error range, in inches
+                                            0, // large error range timeout, in milliseconds
+                                            0 // maximum acceleration (slew)
 );
 
 // angular motion controller
-lemlib::ControllerSettings angularController(2, // proportional gain (kP)
+lemlib::ControllerSettings angularController(0.5, // proportional gain (kP)
                                              0, // integral gain (kI)
-                                             10, // derivative gain (kD)
-                                             3, // anti windup
+                                             0.1, // derivative gain (kD)
+                                             0, // anti windup
                                              1, // small error range, in degrees
-                                             100, // small error range timeout, in milliseconds
-                                             3, // large error range, in degrees
-                                             500, // large error range timeout, in milliseconds
+                                             0, // small error range timeout, in milliseconds
+                                             0, // large error range, in degrees
+                                             0, // large error range timeout, in milliseconds
                                              0 // maximum acceleration (slew)
 );
 
-// sensors for odometry
+// sensors for odometry (set to nullptr if not present)
 lemlib::OdomSensors sensors(&vertical, // vertical tracking wheel
                             nullptr, // vertical tracking wheel 2, set to nullptr as we don't have a second one
                             &horizontal, // horizontal tracking wheel
@@ -82,28 +80,51 @@ lemlib::Chassis chassis(drivetrain, linearController, angularController, sensors
 //Add motors and sensors here
 pros::Motor intake1(21, pros::MotorGearset::blue);
 
-pros::Task screenTask([&]() {
-        while (true) {
-            // print robot location to the brain screen
-            pros::lcd::print(0, "X: %f", chassis.getPose().x); // x
-            pros::lcd::print(1, "Y: %f", chassis.getPose().y); // y
-            pros::lcd::print(2, "Theta: %f", chassis.getPose().theta); // heading
-            // log position telemetry
-            lemlib::telemetrySink()->info("Chassis pose: {}", chassis.getPose());
-            // delay to save resources
-            pros::delay(50);
-        }
-});
+void screenTask() {
+    while (true) {
+        // print robot location to the brain screen
+        pros::lcd::print(0, "X: %f", chassis.getPose().x); // x
+        pros::lcd::print(1, "Y: %f", chassis.getPose().y); // y
+        pros::lcd::print(2, "Theta: %f", chassis.getPose().theta); // heading
+        // log position telemetry
+        lemlib::telemetrySink()->info("Chassis pose: {}", chassis.getPose());
+        // delay to save resources
+        pros::delay(50);
+    }
+}
 
 void initialize() {
     pros::lcd::initialize(); // initialize brain screen
+    pros::Task screenTask();
     chassis.calibrate(); // calibrate sensors
 }
+
+int auton = 0; //what auton you want to run
 
 void autonomous() {
 	chassis.setPose(0,0,0);
 	
-	run_auton();
+	switch(auton)
+    {
+        case 0:
+            auton_example();
+            break;
+        case 1:
+            //another auton function
+            break;
+
+        //add more cases for more autons
+    }
+}
+
+//Make more of these functions for each auton
+void auton_example()
+{
+    chasis.setPose(0, 0, 0); // set starting position (x, y, theta)
+
+    pros::delay(1000); // wait example
+    
+    chasis.moveToPoint(24, 0, 90); // move to point (x, y, theta)
 }
 
 void opcontrol() {
